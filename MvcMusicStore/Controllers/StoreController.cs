d11daf0054b1db048da45a98fde76a032e5ac11d
Madisen Arurang <madireyn@uw.edu>
2018-07-10 09:23:47 -0700

    yep

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+        //
+        // GET: /Store/Browse
+        public string Browse(string genre)
+        {
+            // HttpUtility used to sanitize user input
+            // prevents users from injecting Javascript into View
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message;
+        }
+        //
+        // GET: /Store/Details
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+    }
+}
---
+++ MvcMusicStore2/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore2.Models;
+
+namespace MvcMusicStore2.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            // making list of genres from SampleData.cs instead now 
+            var genres = storeDB.Genres.ToList(); 
+            return View(genres);
+        }
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
